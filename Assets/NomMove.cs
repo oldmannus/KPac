e090edf08a93bf13c332ce0df7b2e1a48b61eddd
oldmannus <durdom3@yahoo.com>
2021-03-16 13:09:30 -0400

    Add files via upload
    The original version of game C and I were playing with.

---
+++ Assets/NomMove.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NomMove : MonoBehaviour
+{ 
+    public float BallSpeed = 1.0f;
+    public KeyCode up, down, left, right;
+
+
+    Collider _collider; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _collider = GetComponent<Collider>();
+        Debug.LogError("HIII");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float deltax = 0;
+        float deltay = 0;
+        float deltaz = 0;
+
+        if (Input.GetKey(down))
+        {
+            deltaz = -Time.deltaTime * BallSpeed;
+        }
+        if (Input.GetKey(up))
+        {
+            deltaz = Time.deltaTime * BallSpeed;
+        }
+        if (Input.GetKey(left))
+        {
+            deltax = -Time.deltaTime * BallSpeed;
+        }
+        if (Input.GetKey(right))
+        {
+            deltax = Time.deltaTime * BallSpeed;
+        }
+
+
+
+        _collider.transform.Translate(new Vector3(deltax, deltay, deltaz));
+
+    }
+}
